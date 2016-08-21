d1237cea03d31a6fca3c5394b6c288f808d3d5e8
chendind <3110100527@zju.edu.cn>
2016-08-21 17:41:01 +0800

    tb

--- source/js/public.js
+++ source/js/public.js
@@ -1,4 +1,12 @@
-mui("body").on('tap','.smallCartIcon',function(e){
-	e.stopPropagation();
-	mui.toast("添加成功");
+$(function(){
+	mui("body").on('tap','.smallCartIcon',function(e){
+		e.stopPropagation();
+		mui.toast("添加成功");
+	});
+	$("body").on('tap','[data-href]',function(e){
+		// e.stopPropagation();
+		e.preventDefault();
+		var href = $(this).attr("data-href");
+		href&&(window.location.href=href);
+	});
 });
