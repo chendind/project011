ae4df091cb16e68c482913c2beb8a9add0c5f0a1
chendind <3110100527@zju.edu.cn>
2016-09-03 13:40:13 +0800

    增加了微信js，增加了微信配置

--- source/js/ajax.js
+++ source/js/ajax.js
@@ -1,9 +1,20 @@
 // 配置项
 var baseUrl = "http://120.26.94.240:8989";
 var contentType = 'application/octet-stream';
+var appId = "wx51e4e7ced9ff48f1";
 function successHandle(data){
-	if((data.resultCode!=200)&&data.message){
+	if(data.resultCode == 402){
+		// 登录超时
+		window.location.href = "login.html";
+		window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid="+appId+
+								"&redirect_uri="+
+								"http://test.qess.me/wechat/getCookie?redirectUrl="+
+								"http://test.qess.me/index.jsp"+
+								"&response_type=code&scope=snsapi_userinfo&connect_redirect=1#wechat_redirect";
+	}
+	else if((data.resultCode!=200)&&data.message){
 		mui.toast(data.message);
+
 	}
 }
 function errorHandle(data){
@@ -29,6 +40,7 @@ function getAjaxHeader(){
 		header.version = "1.0";
 		header.OriginType = "mp";
 	}
+	header.token = "e53ef4ac5eb017417b17aa31c947d683";
 	return header;
 }
 function dataFilter(data,dataType){
@@ -207,9 +219,56 @@ function sendMessageForApplyCeo(phone){
 	});
 	return ajax;
 }
+// 抢购详情接口
+function getPanicBuy(){
+	var ajax = $.ajax({
+		url: baseUrl + "/panicBuy",
+		type: "GET"
+	});
+	return ajax;
+}
+// 获取微信分享参数
+function getWechatShareConfig(url){
+	var ajax = $.ajax({
+		url: baseUrl + "/wxshare/jsapi",
+		data: {
+			"url": url
+		},
+		type: "POST"
+	});
+	return ajax;
+}
+// 设置微信分享参数
+function setWechatShareConfig(title,desc,link,imgUrl){
+	title||(title = '云书，一个免费送教材的平台');
+	desc||(desc = '打破常规，突破垄断；定义教材，预见未来');
+	link||(link = window.location.href);
+	imgUrl||(imgUrl = "http://www.chendind.com/project011/source/images/ysdownload.png");
+    var config = {
+        title: title,
+        desc: desc,
+        link: link,
+        imgUrl: imgUrl,
+        success: function() {
+            
+        },
+        cancel: function() {
 
-
-
+        }
+    };
+    wx.ready(function(data) {
+        // 朋友圈
+        wx.onMenuShareTimeline(config);
+        // 朋友
+        wx.onMenuShareAppMessage(config);
+        // QQ
+        wx.onMenuShareQQ(config);
+        // 腾讯微博
+        wx.onMenuShareWeibo(config);
+        // QQ空间
+        wx.onMenuShareQZone(config);
+    });
+}
 
 
 
--- source/js/public.js
+++ source/js/public.js
@@ -13,6 +13,19 @@ $(function(){
 		var href = $(this).attr("data-href");
 		href&&(window.location.href=href);
 	});
+    $.when(getWechatShareConfig(window.location.href)).done(function(data){
+        if(data.resultCode == 200){
+            wx.config({
+                debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                appId: appId, // 必填，公众号的唯一标识
+                timestamp: data.data.timestamp, // 必填，生成签名的时间戳
+                nonceStr: data.data.nonceStr, // 必填，生成签名的随机串
+                signature: data.data.signature,// 必填，签名，见附录1
+                jsApiList: ["onMenuShareTimeline", "onMenuShareAppMessage", "onMenuShareQQ", "onMenuShareWeibo", "onMenuShareQZone"] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+            });
+            setWechatShareConfig("","","","");
+        }
+    })
 });
 function encodeObj(obj){
 	return encodeURI(JSON.stringify(obj));
@@ -30,10 +43,6 @@ function getQueryData(){
     }
     return parseURI;
 }
-
-
-
-
 /* 一些书本列表页面公用的方法
 */
 var bookTableCellHtml = '<ul class="mui-table-view" style="margin-top: 10px;">'+
